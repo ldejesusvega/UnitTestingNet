5a5a0a8614f25374edee309c54389b26a75b78af
Luis Enrique De jesus Vega <luis.vega@unosquare.com>
2021-10-19 20:48:08 -0500

    Section1 Complete

---
+++ S1/Calculations.Test/CalculatorTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Calculations.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
