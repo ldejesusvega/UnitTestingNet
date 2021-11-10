1f75d682c02c196cbe8e4e5bf936df5dfd4d9ebd
Luis Enrique De Jesus Vega <ldejesusvega@gmail.com>
2021-11-10 03:38:46 -0600

    * CalculatorTest.cs: * Calculations.Test.csproj: * Calculator.cs: Assert Numeric Values

--- S1/Calculations.Test/CalculatorTest.cs
+++ S1/Calculations.Test/CalculatorTest.cs
@@ -1,4 +1,6 @@
+using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Calculations.Test
 {
@@ -7,7 +9,57 @@ namespace Calculations.Test
         [Fact]
         public void TestAdd()
         {
-            Assert.True(true);
+            Assert.Equal(2, 2); // Wrong
+
+            //Assert.Equal({Expected Value }, { Actual Value});
         }
+
+        [Fact]
+        public void Add_GivenTwoIntValues_ReturnsInt()
+        {
+            var calc = new Calculator();
+            var result = calc.Add(1, 2);
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Add_GivenTwoDoubleValues_ReturnsDouble()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.23, 3.51);
+            // Assert Numeric Values 
+            //Assert.Equal(4.7, result); // compare result complete
+            // To round number amd test result add length to compare
+            Assert.Equal(4.6, result, 0); // Compara solo la parte entera
+
+
+            Assert.Equal(4.7, result, 1); // Compara basado en redonde de 1 digito
+        }
+
+        [Fact]
+        public void Add_GivenTwoDoubleValues_ReturnsDouble_CompareUnits()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.23, 3.51);
+            // Assert Numeric Values 
+            //Assert.Equal(4.7, result); // compare result complete
+            // To round number amd test result add length to compare
+            Assert.Equal(4.7, result, 0); // Compara solo la parte entera
+        }
+
+
+        [Fact]
+        public void Add_GivenTwoDoubleValues_ReturnsDouble_OneDigitRound()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.23, 3.51);
+            // Assert Numeric Values 
+            //Assert.Equal(4.7, result); // compare result complete
+            // To round number amd test result add length to compare
+            // Only compares 4.7 instead result 4.74
+            Assert.Equal(4.7, result, 1); // Compara basado en redonde de 1 digito
+        }
+
     }
 }
--- S1/Calculations/Calculator.cs
+++ S1/Calculations/Calculator.cs
@@ -1,8 +1,13 @@
 ﻿namespace Calculations
 {
-    public    class Calculator
+    public class Calculator
     {
         public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double AddDouble(double a, double b)
         {
             return a + b;
         }
