396ece70e07fe88dd184787f3e29572f3670be40
Luis Enrique De Jesus Vega <ldejesusvega@gmail.com>
2021-11-17 18:44:08 -0600

    Add different ways to valisate integer and double values.

--- S1/Calculations.Test/CalculatorTest.cs
+++ S1/Calculations.Test/CalculatorTest.cs
@@ -9,7 +9,7 @@ namespace Calculations.Test
         [Fact]
         public void TestAdd()
         {
-            Assert.Equal(2, 2); // Wrong
+            Assert.Equal(2, 2);
 
             //Assert.Equal({Expected Value }, { Actual Value});
         }
@@ -19,7 +19,8 @@ namespace Calculations.Test
         {
             var calc = new Calculator();
             var result = calc.Add(1, 2);
-
+            // Equal({Expected Value }, { Actual Value})
+            // Equal compares this two values, throws EqualException when object are not equal.  
             Assert.Equal(3, result);
         }
 
@@ -28,13 +29,10 @@ namespace Calculations.Test
         {
             var calc = new Calculator();
             var result = calc.AddDouble(1.23, 3.51);
-            // Assert Numeric Values 
-            //Assert.Equal(4.7, result); // compare result complete
-            // To round number amd test result add length to compare
-            Assert.Equal(4.6, result, 0); // Compara solo la parte entera
-
-
-            Assert.Equal(4.7, result, 1); // Compara basado en redonde de 1 digito
+            // Assert Numeric Values, this compare based on addition result
+            // Equal({Expected Value }, { Actual Value})
+            // Equal compares this two values, throws EqualException when object are not equal.
+            Assert.Equal(4.74, result);
         }
 
         [Fact]
@@ -43,11 +41,13 @@ namespace Calculations.Test
             var calc = new Calculator();
             var result = calc.AddDouble(1.23, 3.51);
             // Assert Numeric Values 
-            //Assert.Equal(4.7, result); // compare result complete
-            // To round number amd test result add length to compare
+            // Equal (decimal expected, decimal actual, int precision);
+            // decimal expected, this is the value we expected from test
+            // decimal actual, this is the value we get from execute method
+            // int precision, this will be decimal positions to compare values
+            // int this case precision is zero, only evaluates units from value 
             Assert.Equal(4.7, result, 0); // Compara solo la parte entera
         }
-
 
         [Fact]
         public void Add_GivenTwoDoubleValues_ReturnsDouble_OneDigitRound()
@@ -55,10 +55,28 @@ namespace Calculations.Test
             var calc = new Calculator();
             var result = calc.AddDouble(1.23, 3.51);
             // Assert Numeric Values 
-            //Assert.Equal(4.7, result); // compare result complete
-            // To round number amd test result add length to compare
-            // Only compares 4.7 instead result 4.74
-            Assert.Equal(4.7, result, 1); // Compara basado en redonde de 1 digito
+            // Equal (decimal expected, decimal actual, int precision);
+            // decimal expected, this is the value we expected from test
+            // decimal actual, this is the value we get from execute method
+            // int precision, this will be decimal positions to compare values
+            // int this case precision is one position, this will compare value
+            // with one position after decimal point.
+            Assert.Equal(4.7, result, 1);
+        }
+
+        [Fact]
+        public void Add_GivenTwoDoubleValues_ReturnsDouble_TwoDigitRound()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.23, 3.51);
+            // Assert Numeric Values 
+            // Equal (decimal expected, decimal actual, int precision);
+            // decimal expected, this is the value we expected from test
+            // decimal actual, this is the value we get from execute method
+            // int precision, this will be decimal positions to compare values
+            // int this case precision is two positions, this will compare value
+            // with two positions after decimal point.
+            Assert.Equal(4.74, result, 2);
         }
 
     }
