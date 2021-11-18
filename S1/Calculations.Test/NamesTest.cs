33aeb95351185082a5fefcb538d480714a20ad68
Luis Enrique De Jesus Vega <ldejesusvega@gmail.com>
2021-11-17 18:45:25 -0600

    * Names.cs: Add this class to test string values
    * NamesTest.cs: Add different ways to test string values

---
+++ S1/Calculations.Test/NamesTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xunit;
+namespace Calculations.Test
+{
+    public class NamesTest
+    {
+        public NamesTest()
+        {
+        }
+
+        [Fact]
+        public void MakeFullNameTest()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("Aref", "Karimi");
+            Assert.Equal("Aref Karimi", result);
+        }
+
+        [Fact]
+        public void MakeFullNameTestIgnoreCase()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("Aref", "Karimi");
+            Assert.Equal("Aref Karimi", result, ignoreCase: true);
+        }
+
+        [Fact]
+        public void MakeFullNameTestContains()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("Aref", "Karimi");
+            Assert.Contains("aref", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void MakeFullNameTestStartsWith()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("Aref", "Karimi");
+            Assert.StartsWith("Aref", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void MakeFiullNameTestEndsWith()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("Aref", "Karimi");
+            Assert.EndsWith("rimi", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void MakeFiullNameTestMatch()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("Aref", "Karimi");
+            Assert.EndsWith("rimi", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+    }
+}
---
+++ S1/Calculations/Names.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Calculations
+{
+    public class Names
+    {
+        public Names()
+        {
+        }
+
+        public string MakeFullName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+
+    }
+}
